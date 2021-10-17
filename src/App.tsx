50fbe6c72fb2eec01bcf6c8d7dc8461ae0110e3e
smartful <remi_rodrigues@hotmail.com>
2021-10-17 02:38:15 +0200

    creation du chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+export interface IMessage {
+  text: string;
+  index: number;
+}
+
+const App: React.FC = () => {
+  const [inputMessage, setInputMessage] = useState<string>('');
+  const [messages, setMessages] = useState<IMessage[]>([]);
+
+  const inputStyle: React.CSSProperties = {
+    width: '30rem',
+    height: '1rem',
+    padding: '1rem',
+    margin: '1rem',
+    display: 'flex',
+    flexDirection: 'column',
+    placeContent: 'center',
+  };
+
+  const btnStyle: React.CSSProperties = {
+    width: '15rem',
+    padding: '1rem',
+    margin: '1rem',
+    display: 'flex',
+    flexDirection: 'column',
+    placeContent: 'center',
+  };
+
+  const msgStyle: React.CSSProperties = {
+    backgroundColor: '#222299',
+    color: 'white',
+    width: '30rem',
+    padding: '1rem',
+    margin: '1rem',
+    display: 'flex',
+    flexDirection: 'column',
+    placeContent: 'center',
+    borderRadius: '2rem',
+  };
+
+  const handleSubmit: (event: React.FormEvent) => void = (event: React.FormEvent) => {
+    event?.preventDefault();
+    setMessages([...messages, { text: inputMessage, index: messages.length}]);
+    setInputMessage('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type='text' style={inputStyle} value={inputMessage} onChange={(e) => setInputMessage(e.target.value)} />
+        <button type='submit' style={btnStyle}>Ajouter un message</button>
+      </form>
+
+      <div style={msgStyle}>
+        { messages.map(message => <p key={message.index}>{message.text}</p>) }
+      </div>
     </div>
   );
 }
